413296c49bcdb2c8827701bf29a9ef2e6f1ee84d
prsyar-byte <dzhwar19999@gmail.com>
2026-07-18 15:28:17 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,27 +1,22 @@
 // firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-
-// زانیاری Firebase ـەکەت لێرە دابنێ
 const firebaseConfig = {
-  apiKey: "API_KEY_لێرە",
-  authDomain: "AUTH_DOMAIN_لێرە",
-  projectId: "PROJECT_ID_لێرە",
-  storageBucket: "STORAGE_BUCKET_لێرە",
-  messagingSenderId: "MESSAGING_SENDER_ID_لێرە",
-  appId: "APP_ID_لێرە"
+  apiKey: "AIzaSyAiMo1BKik6ReqpDdPguJKk61mLGMu_fro",
+  authDomain: "quizapp-5c9db.firebaseapp.com",
+  projectId: "quizapp-5c9db",
+  storageBucket: "quizapp-5c9db.firebasestorage.app",
+  messagingSenderId: "862135431210",
+  appId: "1:862135431210:web:4f848e6af03593bc1b5353",
+  measurementId: "G-S7DJPL4SFW"
 };
 
-
-// دەستپێکردنی Firebase
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// بەکارهێنانی Auth و Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export { auth, db };
