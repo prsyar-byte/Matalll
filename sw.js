2299be29a640f5263aa5d4653fd97db1d5576454
prsyar-byte <dzhwar19999@gmail.com>
2026-07-21 15:07:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,5 @@
-const CACHE_NAME = 'deftar-qarz-v1';
-const urlsToCache = [
-  '/Matalll/',
-  '/Matalll/index.html',
-  '/Matalll/manifest.json',
-  '/Matalll/icons/icon-192.png',
-  '/Matalll/icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    }).catch(() => caches.match('/Matalll/index.html'))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
+const CACHE='deftar-v1';
+const URLS=['/Matalll/','/Matalll/index.html','/Matalll/manifest.json','/Matalll/icons/icon-192.png','/Matalll/icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(f=>caches.open(CACHE).then(c=>{c.put(e.request,f.clone());return f}))).catch(()=>caches.match('/Matalll/index.html')))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))))});
